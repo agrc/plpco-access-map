456d4a40ce0a2d2b7babbd3c52d6513662bbe631
stdavis <stdavis@utah.gov>
2022-06-02 14:50:47 -0600

    chore: June 🌲

--- src/Video.stories.js
+++ src/Video.stories.js
@@ -2,12 +2,18 @@ import React from 'react';
 import Video from './Video';
 import './VideosContainer.scss';
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   title: 'Video',
   component: Video
 }
 
 
+const mockPoint = {
+  attributes: {
+    'Date_Time': '2020-01-01T00:00:00.000Z',
+  }
+};
 const mockProps = {
   RD_ID: "RD200225",
   GPS_Track_ID: "RD200225a",
@@ -17,7 +23,8 @@ const mockProps = {
   Road_Condition: null,
   URL: "https://youtu.be/F6ONE_NnSRA",
   Notes: "When this Redo uploaded, delete the report/data on 9/7/2017.",
-  mapView: { graphics: { add: () => {} } }
+  mapView: { graphics: { add: () => {}, remove: () => {} } },
+  pointsLayer: { queryFeatures: async () => ({ exceededTransferLimit: false, features: [mockPoint]}) },
 };
 
 export const DefaultVideo = () => (
