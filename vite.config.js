a294852e4a4df258f8d9ab3af82ef35e444adab8
stdavis <stdavis@utah.gov>
2022-06-28 15:47:00 -0600

    fix: deploy-specific document titles
    Related to #38

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import vitePluginHtmlEnv from 'vite-plugin-html-env';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vitePluginHtmlEnv({ prefix: '{{', suffix: '}}' })],
   base: './',
 });
