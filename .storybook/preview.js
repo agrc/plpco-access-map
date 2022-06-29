bab1f6a0386c7d18a5d558ba40731a61c24c65db
stdavis <stdavis@utah.gov>
2022-06-29 14:37:31 -0600

    refactor: point at local esri js api assets

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import '../src/index.css';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
+import '@arcgis/core/assets/esri/themes/light/main.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/js/dist/collapse';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './index.css';
 
 createRoot(document.getElementById('root')).render(<App />);
