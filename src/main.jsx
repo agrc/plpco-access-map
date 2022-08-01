51cf46c327b98db3846df13d0fb25bc6d44aca20
Scott Davis <stdavis@utah.gov>
2022-08-01 10:01:07 -0600

    chore: Aug dep bumps 🌲 (#52)

--- src/main.jsx
+++ src/main.jsx
@@ -4,4 +4,5 @@ import 'bootstrap/js/dist/collapse';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
+document.title = import.meta.env.VITE_APP_TITLE;
 createRoot(document.getElementById('root')).render(<App />);
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import vitePluginHtmlEnv from 'vite-plugin-html-env';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vitePluginHtmlEnv({ prefix: '{{', suffix: '}}' })],
+  plugins: [react()],
   base: './',
 });
